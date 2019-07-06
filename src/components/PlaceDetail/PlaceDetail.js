da739f209977675f20712740f60402c45151c0d6
Sophearychiv <sopheary.chiv@gmail.com>
2019-07-06 02:19:14 -0700

    added icons

--- src/components/PlaceDetail/PlaceDetail.js
+++ src/components/PlaceDetail/PlaceDetail.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { Modal, View, Image, Text, Button, StyleSheet } from "react-native";
-
+import { Modal, View, Image, Text, Button, StyleSheet, TouchableOpacity } from "react-native";
+import Icon from 'react-native-vector-icons/Ionicons';
 const placeDetail = props => {
   let modalContent = null;
 
@@ -21,7 +21,11 @@ const placeDetail = props => {
       <View style={styles.modalContainer}>
         {modalContent}
         <View>
-          <Button title="Delete" color="red" onPress={props.onItemDeleted} />
+          <TouchableOpacity onPress={props.onItemDeleted}>
+            <View style={styles.deleteIcon}> 
+              <Icon size={30} name="ios-trash" color="red"/>
+            </View>
+          </TouchableOpacity>
           <Button title="Close" onPress={props.onModalClosed} />
         </View>
       </View>
@@ -41,6 +45,9 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     textAlign: "center",
     fontSize: 28
+  },
+  deleteIcon: {
+    alignItems: "center"
   }
 });
 
