894310a80a0a69a2deccd5e02118a1fa6f13042e
Sophearychiv <sopheary.chiv@gmail.com>
2019-07-06 14:29:54 -0700

    navigatign forward and backward

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import AuthScreen from './src/screens/Auth/Auth';
 import SharePlaceScreen from './src/screens/SharePlace/SharePlace';
 import FindPlaceScreen from './src/screens/FindPlace/FindPlace';
 import configureStore from './src/store/configureStore';
+import PlaceDetailScreen from './src/screens/PlaceDetail/PlaceDetail';
 
 const store = configureStore();
 
@@ -11,7 +12,7 @@ const store = configureStore();
 Navigation.registerComponent("awesome-places.AuthScreen", () => AuthScreen, store, Provider);
 Navigation.registerComponent("awesome-places.SharePlaceScreen", () => SharePlaceScreen, store, Provider);
 Navigation.registerComponent("awesome-places.FindPlaceScreen", () => FindPlaceScreen, store, Provider);
-
+Navigation.registerComponent("awesome-places.PlaceDetailScreen", () => PlaceDetailScreen, store, Provider);
 // Start App
 Navigation.startSingleScreenApp({
   screen: {
