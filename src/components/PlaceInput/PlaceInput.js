7202c7e82492b340e9982dc69245d362f7044826
Sophearychiv <sopheary.chiv@gmail.com>
2019-07-07 00:14:46 -0700

    finished Share Place component

--- src/components/PlaceInput/PlaceInput.js
+++ src/components/PlaceInput/PlaceInput.js
@@ -1,23 +1,14 @@
 import React, { Component } from "react";
 import { View, TextInput, Button, StyleSheet } from "react-native";
 import DefaultInput from '../UI/DefaultInput/DefaultInput';
-class PlaceInput extends Component {
-  state = {
-    placeName: ""
-  };
 
-  placeNameChangedHandler = val => {
-    this.setState({
-      placeName: val
-    });
-  };
+const PlaceInput = props => (
+  <DefaultInput 
+    placeholder="Place Name" 
+    value={props.placeName} 
+    onChangeText={props.onChangeText}
+  />
+);
 
-  render() {
-    return (
-        <DefaultInput placeholder="Place Name" value={this.state.placeName} onChangeText={this.placeNameChangedHandler}/>
-
-    );
-  }
-}
 
 export default PlaceInput;
