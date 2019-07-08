2daf05517147feb10a4120c32432f9dc6ffb3595
Sophearychiv <sopheary.chiv@gmail.com>
2019-07-07 21:58:39 -0700

    responsive design for Auth screen

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {View, Button, StyleSheet, ImageBackground} from 'react-native';
+import {View, StyleSheet, ImageBackground, Dimensions} from 'react-native';
 import startMainTabs from '../MainTabs/startMainTabs';
 import DefaultInput from '../../components/UI/DefaultInput/DefaultInput';
 import HeadingText from '../../components/UI/HeadingText/HeadingText';
@@ -7,24 +7,65 @@ import MainText from '../../components/UI/MainText/MainText';
 import backgroundImage from '../../assets/beautiful-place.jpg';
 import ButtonWithBackground from '../../components/UI/ButtonWithBackground/ButtonWithBackground';
 class AuthScreen extends Component {
+    state = {
+        viewMode: Dimensions.get("window").height > 500 ? "portrait" : "landscape"
+    }
+
+    constructor(props) {
+        super(props);
+        Dimensions.addEventListener("change", (dims) => {
+            this.setState({
+                viewMode: Dimensions.get("window").height > 500 ? "portrait" : "landscape"
+            })
+        });
+    }
 
     loginHandler = () => {
         startMainTabs();
     }
     render() {
+        let headingText = null;
+        if (this.state.viewMode === "portrait") {
+            headingText = (
+                <MainText>
+                    <HeadingText>Please Login</HeadingText>
+                </MainText>
+            );
+        }
         return (
             <ImageBackground source={backgroundImage} style={styles.backgroundImage}>
                 <View style={styles.container}>
-                        <MainText>
-                            <HeadingText>Please Login</HeadingText>
-                        </MainText>
-                        <ButtonWithBackground color="#29aaf4" onPress={this.loginHandler}>Switch to Login</ButtonWithBackground>
+                        {headingText}
+                        <ButtonWithBackground 
+                            color="#29aaf4" 
+                            onPress={this.loginHandler}
+                        >
+                                Switch to Login
+                        </ButtonWithBackground>
                         <View style={styles.inputContainer}> 
                             <DefaultInput placeholder="Your email address" style={styles.input}/>
-                            <DefaultInput placeholder="Password" style={styles.input}/>
-                            <DefaultInput placeholder="Confirm Password" style={styles.input}/>
+                            <View style={this.state.viewMode === "portrait" ? 
+                                styles.portraitPasswordContainer : 
+                                styles.landscapePasswordContainer}>
+                                <View style={this.state.viewMode === "portrait" ? 
+                                    styles.portraitPasswordWrapper : 
+                                    styles.landscapePasswordWrapper}
+                                >
+                                    <DefaultInput placeholder="Password" style={styles.input}/>
+                                </View>
+                                <View style={this.state.viewMode === "portrait" ? 
+                                    styles.portraitPasswordWrapper : 
+                                    styles.landscapePasswordWrapper}
+                                >
+                                    <DefaultInput placeholder="Confirm Password" style={styles.input}/>
+                                </View>
+                            </View>
                         </View>
-                        <ButtonWithBackground color="#29aaf4" onPress={this.loginHandler}> Submit </ButtonWithBackground>
+                        <ButtonWithBackground 
+                            color="#29aaf4" 
+                            onPress={this.loginHandler}> 
+                            Submit 
+                        </ButtonWithBackground>
                 </View>
             </ImageBackground>
         );
@@ -47,6 +88,20 @@ const styles = StyleSheet.create({
     input: {
         backgroundColor: "#eee",
         borderColor: "#bbb"
+    },
+    landscapePasswordContainer: {
+        flexDirection: "row",
+        justifyContent: "space-between"
+    },
+    portraitPasswordContainer: {
+        flexDirection: "column",
+        justifyContent: "flex-start"
+    },
+    landscapePasswordWrapper: {
+        width: "45%"
+    },
+    portraitPasswordWrapper: {
+        width: "100%"
     }
 });
 export default AuthScreen;
